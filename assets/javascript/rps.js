3f77ab31bdc7d9c165abecb8e6ae1779cc854e3f
Haakasaurus <kramerhjohnson@gmail.com>
2019-09-04 16:43:59 -0700

    Beautified the file.

--- assets/javascript/rps.js
+++ assets/javascript/rps.js
@@ -1,9 +1,9 @@
 // Function to get the value of the users choice in the radio button form.
-let getUserRPSChoice = function() {
+let getUserRPSChoice = function () {
     // Create an array of the values of the checkboxes.
     let userChoiceRadio = document.forms['playForm'].elements['rps'];
     // Loop over each item in the array to see which one is checked.
-    for (i=0; i < userChoiceRadio.length; i++) {
+    for (i = 0; i < userChoiceRadio.length; i++) {
         if (userChoiceRadio[i].checked) {
             // Store the value of the checked radio button and end the function. 
             let rpsUser = userChoiceRadio[i].value;
@@ -17,7 +17,7 @@ let getUserRPSChoice = function() {
 
 /* Create a random number between 0-1.  Multiply by 3 and round down to result 
  in a number between 1-2. */
-let getRandomNumber = function() {
+let getRandomNumber = function () {
     let randomNumber = Math.floor(Math.random() * 3);
     return randomNumber;
 }
@@ -27,16 +27,16 @@ let getRandomNumber = function() {
 
 /* Create counter functions to display the 
 number of games played, wins, losses, and ties. */
-let gameCounter = function() {
+let gameCounter = function () {
     document.getElementById('game-count').innerHTML = parseInt(document.getElementById('game-count').innerHTML) + 1;
 }
-let winCount = function() {
+let winCount = function () {
     document.getElementById('wins').innerHTML = parseInt(document.getElementById('wins').innerHTML) + 1;
 }
-let lossCount = function() {
+let lossCount = function () {
     document.getElementById('losses').innerHTML = parseInt(document.getElementById('losses').innerHTML) + 1;
 }
-let tieCount = function() {
+let tieCount = function () {
     document.getElementById('ties').innerHTML = parseInt(document.getElementById('ties').innerHTML) + 1;
 }
 
@@ -44,7 +44,7 @@ let tieCount = function() {
 
 /* Function to find out if the user's choice wins, loses, or ties with the 
 computer's choice. */
-let winLoseTie = function(choice, number) {
+let winLoseTie = function (choice, number) {
     let rock = 'rock';
     let paper = 'paper';
     let scissors = 'scissors'
@@ -80,16 +80,16 @@ let winLoseTie = function(choice, number) {
         // Let the user know they won.
         document.getElementById('result').setAttribute('value', 'You win!');
         winCount();
-    }
-    /* Check if the value associated with the user's choice in lossObj is the 
-     same as the computer's choice. */
-    else if (lossObj[choice] === rpsComp) {
+
+        /* Check if the value associated with the user's choice in lossObj is the 
+         same as the computer's choice. */
+    } else if (lossObj[choice] === rpsComp) {
         // Let the user know they lost.
         document.getElementById('result').setAttribute('value', 'You lose...');
         lossCount();
-    }
-    // No need to check if there is a tie as that is the only remaining option.
-    else {
+
+        // No need to check if there is a tie as that is the only remaining option.
+    } else {
         // Let the user know they tied.
         document.getElementById('result').setAttribute('value', 'It\'s a tie!');
         tieCount();
