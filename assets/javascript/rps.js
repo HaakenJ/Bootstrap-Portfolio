cc64302e6a5b1924d18abc20cad69571273c5f55
Haakasaurus <kramerhjohnson@gmail.com>
2019-09-04 14:06:26 -0700

    Cleaned up variable names, formatting, and syntax of rps.js.

--- assets/javascript/rps.js
+++ assets/javascript/rps.js
@@ -1,13 +1,12 @@
 // Function to get the value of the users choice in the radio button form.
-let choiceFunction = function() {
+let getUserRPSChoice = function() {
     // Create an array of the values of the checkboxes.
-    let radio = document.forms['playForm'].elements['rps'];
+    let userChoiceRadio = document.forms['playForm'].elements['rps'];
     // Loop over each item in the array to see which one is checked.
-    for (i=0; i < radio.length; i++) {
-        if (radio[i].checked) {
+    for (i=0; i < userChoiceRadio.length; i++) {
+        if (userChoiceRadio[i].checked) {
             // Store the value of the checked radio button and end the function. 
-            // Only one radio button can be checked.
-            let rpsUser = radio[i].value;
+            let rpsUser = userChoiceRadio[i].value;
             return rpsUser;
         }
     }
@@ -16,79 +15,74 @@ let choiceFunction = function() {
 
 
 
-// Create a random number between 0-1.  Multiply by 3 and round down to result 
-// in a number between 1-2.
-let randomNumber = function() {
-    let number = Math.floor(Math.random() * 3);
-    return number;
+/* Create a random number between 0-1.  Multiply by 3 and round down to result 
+ in a number between 1-2. */
+let getRandomNumber = function() {
+    let randomNumber = Math.floor(Math.random() * 3);
+    return randomNumber;
 }
 
 
 
 
-// Create counter functions to display the 
-// number of games played, wins, losses, and ties.
-let count = function() {
-    let numOfGames = document.getElementById('count').innerHTML;
-    numOfGames++
-    document.getElementById('count').innerHTML = numOfGames;
+/* Create counter functions to display the 
+number of games played, wins, losses, and ties. */
+let gameCounter = function() {
+    document.getElementById('game-count').innerHTML = parseInt(document.getElementById('game-count').innerHTML) + 1;
 }
 let winCount = function() {
-    let numOfWins = document.getElementById('wins').innerHTML;
-    numOfWins++
-    document.getElementById('wins').innerHTML = numOfWins;
+    document.getElementById('wins').innerHTML = parseInt(document.getElementById('wins').innerHTML) + 1;
 }
 let lossCount = function() {
-    let numOfLosses = document.getElementById('losses').innerHTML;
-    numOfLosses++
-    document.getElementById('losses').innerHTML = numOfLosses;
+    document.getElementById('losses').innerHTML = parseInt(document.getElementById('losses').innerHTML) + 1;
 }
 let tieCount = function() {
-    let numOfTies = document.getElementById('ties').innerHTML;
-    numOfTies++
-    document.getElementById('ties').innerHTML = numOfTies;
+    document.getElementById('ties').innerHTML = parseInt(document.getElementById('ties').innerHTML) + 1;
 }
 
 
 
-// Function to find out if the user's choice wins, loses, or ties with the 
-// computer's choice.
+/* Function to find out if the user's choice wins, loses, or ties with the 
+computer's choice. */
 let winLoseTie = function(choice, number) {
+    let rock = 'rock';
+    let paper = 'paper';
+    let scissors = 'scissors'
 
-    // Array of choices gets indexed using the computer's random number to give
-    // the computer's choice.
-    let options = ['rock', 'paper', 'scissors'];
+    /* Array of choices gets indexed using the computer's random number to give
+    the computer's choice. */
+    let options = [rock, paper, scissors];
     let rpsComp = options[number];
 
     // Display the computer's choice to the user, no suspicion of foul play here!
     document.getElementById('compChoice').setAttribute('value', rpsComp);
 
-    // Defining an object with a user's choice as the property and what value 
-    // is required for that choice to result in a win.
+    /* Defining an object with a user's choice as the property and what value 
+     is required for that choice to result in a win. */
 
     var winObj = {
-        rock:'scissors',
-        paper:'rock',
-        scissors:'paper'
+        rock: scissors,
+        paper: rock,
+        scissors: paper
     };
 
-    // Defining an object with a user's choice as the property and what value 
-    // is required for that choice to result in a loss.
+    /* Defining an object with a user's choice as the property and what value 
+     is required for that choice to result in a loss. */
     var lossObj = {
-    rock:'paper',
-    paper:'scissors',
-    scissors:'rock'
+        rock: paper,
+        paper: scissors,
+        scissors: rock
     };
 
-    // Check if the value associated with the user's choice in winObj is the 
-    // same as the computer's choice.
+    /* Check if the value associated with the user's choice in winObj is the 
+     same as the computer's choice. */
     if (winObj[choice] === rpsComp) {
         // Let the user know they won.
         document.getElementById('result').setAttribute('value', 'You win!');
         winCount();
     }
-    // Check if the value associated with the user's choice in lossObj is the 
-    // same as the computer's choice.
+    /* Check if the value associated with the user's choice in lossObj is the 
+     same as the computer's choice. */
     else if (lossObj[choice] === rpsComp) {
         // Let the user know they lost.
         document.getElementById('result').setAttribute('value', 'You lose...');
